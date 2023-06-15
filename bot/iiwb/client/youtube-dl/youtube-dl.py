e85e4cb5cdc90d02556ce65bd15ba8e6c196b4b0
Hveodrungr <youssef.zaagougui@viacesi.fr>
2023-06-15 16:01:25 +0200

    Fix audio streaming cutting off

--- bot/iiwb/client/youtube-dl/youtube-dl.py
+++ bot/iiwb/client/youtube-dl/youtube-dl.py
@@ -19,7 +19,8 @@ ytdl_format_options = {
     'quiet': True,
     'no_warnings': True,
     'default_search': 'auto',
-    'source_address': '0.0.0.0',  # bind to ipv4 since ipv6 addresses cause issues sometimes
+    'source_address': '0.0.0.0', 
+	# bind to ipv4 since ipv6 addresses cause issues sometimes
     'postprocessors': [{
                 'key': 'FFmpegExtractAudio',
                 'preferredcodec': 'mp3',
@@ -28,7 +29,8 @@ ytdl_format_options = {
 }
 
 ffmpeg_options = {
-    'options': '-vn',
+    'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
+	'options': '-vn',
 }
 
 ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
