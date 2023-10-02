aa7892213b3a61cb235d2d31978c77f56adc2871
Hveodrungr <youssef.zaagougui@viacesi.fr>
2023-10-02 14:13:24 +0200

    Commentaire

--- bot/iiwb/bot/app.py
+++ bot/iiwb/bot/app.py
@@ -10,11 +10,27 @@ import signal
 import time
 
 class Bot(Reverse):
+	"""Bot class for IIWB"""
 	
 	def __new__(cls, command_prefix, description=None, **kwargs):
+		"""__new__
+
+		Parameters
+		----------
+		command_prefix : str
+			Prefix used by the bot
+		description : str, optional
+			description, by default None
+
+		Returns
+		-------
+		None
+		"""
 		return super(Bot, cls).__new__(cls)
 
 	def __init__(self, command_prefix, description=None, **kwargs):
+		"""__int__
+		"""
 		super().__init__(command_prefix, description, **kwargs)
 		sys.tracebacklimit = 1
 		self.prefix = command_prefix
@@ -24,20 +40,44 @@ class Bot(Reverse):
 		self.isShutingdown = False
 
 	def registerEvents(self):
+		"""Register events
+		"""
 		self.getClient().event(self.on_ready)
 		self.getClient().event(self.on_message)
 
 	async def on_ready(self, ctx=None):
+		"""on_ready
+
+		Parameters
+		----------
+		ctx : context, optional
+		"""		
 		self.instance.tree.copy_global_to(guild= discord.Object(id=1101948059270791318))
 		await self.instance.tree.sync(guild= discord.Object(id=1101948059270791318))
 		print(f'Using {__title__} ver.{__version__} aka {__codename__} - Discord.py ver.{dversion}')
 		print(f'Protocol {__fullcodename__}')
 	
 	async def run(self, token: str, status: str = "starting"):
+		"""Start bot
+
+		Parameters
+		----------
+		token : str
+			Discord Token
+		status : str, optional
+			_description_, by default "starting"
+		"""
 		await super().run(token=token)
 		print("{} successfully".format(status))
 
 	def handler(signum, frame):
+		"""Listener
+
+		Parameters
+		----------
+		signum : str
+		frame : str
+		"""
 		res = input("Ctrl-c was pressed. Do you really want to exit? y/n ")
 		if res == 'y':
 			exit(1)
@@ -45,6 +85,7 @@ class Bot(Reverse):
 	signal.signal(signal.SIGINT, handler)
 	
 	async def isShutingdown(self):
+		"""Check if bot is shutting down"""
 		return self.isShutingdown
 				
 		
