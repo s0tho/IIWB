2c6f6e98e2d0461fe916a8af45392467080e364d
Hveodrungr <youssef.zaagougui@outlook.com>
2025-04-17 18:48:20 +0200

    Update the launcher, get token from _env

--- bot/Engine.py
+++ bot/Engine.py
@@ -1,11 +1,34 @@
 import subprocess
 import sys
+from iiwb import _env
 
-filename = 'run.py'
+
+# Push IIWB's Token
+sys.argv.insert(1, _env.TOKEN)
+
 while True:
-    p = subprocess.Popen('python {} {}'.format(filename, *sys.argv[1:]), shell=True).wait()
+    p = subprocess.Popen('python {} {}'.format(_env.IIWB_MAIN, *sys.argv[1:]), shell=True).wait()
 
     if p != 0:
         continue
     else:
         break
+
+
+"""
+import argparse
+
+
+if __name__ == "__main__":
+
+   cmd_parser = argparse.ArgumentParser(description="Discord's bot IIWB")
+   cmd_parser.add_argument("-t", "--token", help="Discord Bot Token")
+   cmd_parser.add_argument("-L", "--log-level",
+                           help="Logging level in [DEBUG,INFO,WARNING,ERROR] (default=INFO)",
+                           default="INFO",
+                           choices=["DEBUG", "INFO", "WARNING", "ERROR"])
+
+   args, unknown = cmd_parser.parse_known_args()
+
+print(args, unknown)
+"""
